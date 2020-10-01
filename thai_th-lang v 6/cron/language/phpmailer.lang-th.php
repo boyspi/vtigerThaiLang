818cf40784ed6462fa2303f4fc82b855024369e8
izcream* <rabbit-dolls@hotmail.com>
2020-10-01 12:35:03 +0700

    Update phpmailer.lang-th.php

--- thai_th-lang v 6/cron/language/phpmailer.lang-th.php
+++ thai_th-lang v 6/cron/language/phpmailer.lang-th.php
@@ -16,16 +16,16 @@
 $PHPMAILER_LANG = array();
 $PHPMAILER_LANG["provide_address"] = 'You must provide at least one ' .
                                      'recipient email address.';
-$PHPMAILER_LANG["mailer_not_supported"] = ' mailer is not supported.';
-$PHPMAILER_LANG["execute"] = 'Could not execute: ';
+$PHPMAILER_LANG["mailer_not_supported"] = ' ระบบ Mailer ไม่ซัพพอร์ต.';
+$PHPMAILER_LANG["execute"] = 'ไม่สามารถรัน: ';
 $PHPMAILER_LANG["instantiate"] = 'Could not instantiate mail function.';
-$PHPMAILER_LANG["authenticate"] = 'SMTP Error: Could not authenticate.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP Error: ไม่สามารถยืนยันตนได้.';
 $PHPMAILER_LANG["from_failed"] = 'The following From address failed: ';
 $PHPMAILER_LANG["recipients_failed"] = 'SMTP Error: The following ' .
                                        'recipients failed: ';
-$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Error: Data not accepted.';
-$PHPMAILER_LANG["connect_host"] = 'SMTP Error: Could not connect to SMTP host.';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Error: รูปแบบข้อมูลไม่ถูกต้อง.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP Error: ไม่สามารถเชื่อมต่อไปยัง SMTP ได้.';
 $PHPMAILER_LANG["file_access"] = 'Could not access file: ';
-$PHPMAILER_LANG["file_open"] = 'File Error: Could not open file: ';
+$PHPMAILER_LANG["file_open"] = 'File Error: ไม่สามารถเปิดไฟล์: ';
 $PHPMAILER_LANG["encoding"] = 'Unknown encoding: ';
 ?>
